3f7f19ef0d012d93eb39c02e257fb374d76a3d1d
Triyonorifan11 <triyonorifan01@gmail.com>
2023-03-28 22:18:30 +0700

    update title atribut html in btn action

--- assets/templates.js
+++ assets/templates.js
@@ -19,13 +19,13 @@ const dataRow = (element) => `
     <td>${element.email}</td>
     <td>${element.website}</td>
     <td>
-        <button type="button" class="btn btn-secondary btn-sm" title="edit data" id="btn_detail" data-bs-toggle="modal" data-id-user="${element.id}" data-bs-target="#exampleModal">
+        <button type="button" class="btn btn-secondary btn-sm" title="detail data" id="btn_detail" data-bs-toggle="modal" data-id-user="${element.id}" data-bs-target="#exampleModal">
             <i class="bi bi-info-circle"></i>
         </button>
         <button type="button" class="btn btn-info btn-sm" title="edit data" id="btn_edit" data-bs-toggle="modal" data-id-user="${element.id}" data-bs-target="#exampleModal">
             <i class="bi bi-pencil-square"></i>
         </button>
-        <button type="button" class="btn btn-danger btn-sm" title="edit data" id="btn_delete"  data-id-user="${element.id}">
+        <button type="button" class="btn btn-danger btn-sm" title="hapus data" id="btn_delete"  data-id-user="${element.id}">
             <i class="bi bi-trash"></i>
         </button>
     </td>
